9b78ba0306cbfe23e607fe49b11c88831a153550
Brendan Aguiar <50648429+brendankaguiar@users.noreply.github.com>
2022-02-24 22:49:18 -0800

    Update SQLB.py

--- SQLB.py
+++ SQLB.py
@@ -1,4 +1,4 @@
-from lib2to3.pgen2.token import NEWLINE
+
 import sys #module used to pass filename argument 
 
 dBn = [] #list to contain databases]
@@ -6,8 +6,8 @@ error1 = 0 #CREATE DATABASE error
 error2 = 0 #DROP DATABASE error
 error3 = 0 #CREATE TABLE error
 error4 = 0 #DROP TABLE error
-inUse = [] #
-class Table:#table class
+inUse = [] #current database in use
+class Table:
     def __init__(self,title,len=None,attr=None,type=None):#default constructor
         if len is None:
             self.len = []
@@ -23,31 +23,56 @@ class Table:#table class
             self.attr.append(vals.pop())
             self.type.append(vals.pop())
             self.len.append(vals.pop())
-        print("Table",self.title, "created.")
         return self
-    def getAttrCount(self):
-        return len(self.attr)
+    def unsetTable(self):
+        del self.title
+        i = len(self.attr) - 1
+        while i >= 0:
+            del self.attr[i]
+            del self.len[i]
+            del self.type[i]
+            i = i - 1        
 
-class DataBase:#Database class
+class DataBase:
     def __init__(self,name):
         self.obj1 = Table([])#Database has a Table
         self.name = name
         self.Tbln = []
         print("Database", name, "created.")
+    
+    def modifyValues(self,tblVals):
+        modType = tblVals.pop(1)#used for different types of modification
+        if modType == "ADD":
+            i = 0
+            for obj in self.Tbln:
+                title = tblVals[0]
+                if obj.title == tblVals[0]:
+                    break#get index of table to modify
+                i = 1 + 1
+            self.Tbln[i] = self.obj1.setTable(tblVals)
+            print("Table", title, "modified.")
+
     def setValues(self,tblVals):
         global error3
         if self.Tbln == []:
+            title = tblVals[0]
             self.Tbln.append(self.obj1.setTable(tblVals))#append to empty table list
+            print("Table", title, "created.")
         else:
             for obj in self.Tbln:
-                tblVals.reverse()
-                if obj.title == tblVals.pop():
-                    print("!Failed to create ", obj.title, "because it already exists.")
+                if obj.title == tblVals[0]:
+                    print("!Failed to create", obj.title, "because it already exists.")
                     error3 = 1
             if error3 == 1:
                 error3 = 0#reset
             else:
+                title = tblVals[0]
+                print(self.Tbln[0].title)
+                self.Tbln[1] = (Table)
                 self.Tbln.append(self.obj1.setTable(tblVals))#append to non-empty table list
+                print(self.Tbln[0].title)
+                print("Table", title, "created.")
+
     def removeTable(self,tblName):
         i = 0
         for obj in self.Tbln:
@@ -55,31 +80,53 @@ class DataBase:#Database class
                 break
             else:
                 i = i + 1#get index of table in use
-        self.Tbln.pop(i)
-        print(len(self.obj1.attr))
+        self.Tbln[i].unsetTable()
+        del self.Tbln[i]
         print("Table", tblName, "deleted.")
         
     def selectTable(self,tblName):
         i = 0
         for obj in self.Tbln:
-            if dBn[i].name == inUse:
+            print(self.Tbln[i].title)
+            if self.Tbln[i].title == tblName:
                 break
             else:
-                i = i + 1#get index of table in use
-        j = self.Tbln[i].getAttrCount()
-        for obj2 in self.Tbln[i].attr:
-            if self.Tbln[i].len[j] != 0:#if using varchar or char
+                i = i + 1#get index of table in use 
+        j = 0
+        while j < len(self.Tbln[i].attr):
+            if self.Tbln[i].len[j] != 0:
                 print(self.Tbln[i].attr[j], self.Tbln[i].type[j], end='')
                 print('(' + str(self.Tbln[i].len[j]) +')', end='')
             else:
                 print(self.Tbln[i].attr[j], self.Tbln[i].type[j], end='')
-            print(' | ', end='')
-            j = j + 1
-            print(j)
+            if j + 1 == len(self.Tbln[i].attr):
+                print(' ', end='\n')
+            else :
+                print(' | ', end='')
         
+            j = j + 1
+def processExitKey():
+    print("All Done.")
+
+def processAlterKey(line):
+    global dBn, inUse
+    line = line.replace("ALTER TABLE ", '')
+    line = line.split(";")[0]
+    line = line.split(" ",-1)
+    line.append(0)
+    i = 0
+    for obj in dBn:
+        if dBn[i].name == inUse:
+            break
+        else:
+            i = i + 1#get index of database in use
+    dBn[i].modifyValues(line)
+
+
+
 def processSelectKey(line):
     line = line.replace("SELECT * FROM ",'')
-    global dBn
+    global dBn, inUse
     temp = line.split(';')[0]
     i = 0
     for obj in dBn:
@@ -87,9 +134,11 @@ def processSelectKey(line):
             break
         else:
             i = i + 1#get index of database in use
-    for tableObj in dBn[i].Tbln:
-                if tableObj.title == temp:
-                    dBn[i].selectTable(temp)        
+    for tableObj in dBn[0].Tbln:
+        if tableObj.title == temp:
+            dBn[i].selectTable(temp)  
+        else:
+            print("!Failed to query table", temp, "because it does not exist.")      
 
 def processUseKey(line):
     line = line.replace("USE ",'')
@@ -99,7 +148,7 @@ def processUseKey(line):
 
 def processDropKey(line): 
     line = line.replace("DROP ",'')
-    global dBn, error2, error4
+    global dBn, error2, error4, inUse
     if line.startswith("DATABASE "):
         line = line.replace("DATABASE ", '')
         temp = line.split(';')[0]
@@ -130,12 +179,13 @@ def processDropKey(line):
                     error4 = 1
             if error4 == 1:
                 error4 = 0#reset
+                print("!Failed to delete", temp, "because it does not exist.")
         elif error4 == 1 or  dBn[i].Tbln == []:
             print("!Failed to delete", temp, "because it does not exist.")
 
 def processCreateKey(line):
     line = line.replace("CREATE ",'')
-    global dBn, error1
+    global dBn, error1, inUse
     if line.startswith("DATABASE "):
         line = line.replace("DATABASE ", '')
         if dBn == []:
@@ -182,7 +232,6 @@ def processCreateKey(line):
             else:
                 i = i + 1#get index of database in use
         dBn[i].setValues(tblVals)
-       # print(tblVals)
 
 def loadDatabase(fname):
     dBfile = open(fname, "r")
@@ -190,14 +239,22 @@ def loadDatabase(fname):
     global inUse
     while curLine:
         curLine = dBfile.readline()
-        if curLine.startswith("CREATE "):
+        if curLine.startswith("CREATE"):
             processCreateKey(curLine)
-        elif curLine.startswith("DROP "):
+        elif curLine.startswith("DROP"):
             processDropKey(curLine)
-        elif curLine.startswith("USE "):
+        elif curLine.startswith("USE"):
             inUse = processUseKey(curLine)
         elif curLine.startswith("SELECT"):
             processSelectKey(curLine)
+        elif curLine.startswith("ALTER"):
+            processAlterKey(curLine)
+        elif curLine.startswith(".EXIT"):
+            processExitKey()
+        elif curLine.startswith('\n'):
+            continue
+        else:
+            break
     dBfile.close()
 
 if len(sys.argv) < 2: #check arguments
